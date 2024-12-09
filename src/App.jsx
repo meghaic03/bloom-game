1622d63fb31114e898c9ae67e035b67196cf7031
Meghai Choudhury <meghaic03@gmail.com>
2024-12-08 21:52:34 -0500

    changed base again

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BloomGame from './components/BloomGame';
 import CommentsPage from './components/CommentsPage';
 
+
 function App() {
   return (
-    <Router>
-      <div className="p-4">
+    <Router basename="/">      <div className="p-4">
         <Routes>
-          <Route path="/bloom" element={<BloomGame />} />
           <Route path="/bloom/comments" element={<CommentsPage />} />
-          <Route path="/" element={<BloomGame />} /> {/* Default route */}
+          <Route path="/bloom" element={<BloomGame />} />
+          {/*<Route path="/" element={<BloomGame />} />  Default route */}
         </Routes>
       </div>
     </Router>
