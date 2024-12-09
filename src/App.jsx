7e6f05a6e1b10e58d3caaa306998e9bade0da8c9
Meghai Choudhury <meghaic03@gmail.com>
2024-12-08 22:24:40 -0500

    added a test page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BloomGame from './components/BloomGame';
 import CommentsPage from './components/CommentsPage';
+import TestPage from './components/test';
 
 
 function App() {
   return (
-    <Router basename="/">      <div className="p-4">
+    <Router basename="/">      
+      <div className="p-4">
         <Routes>
           <Route path="/bloom/comments" element={<CommentsPage />} />
           <Route path="/bloom" element={<BloomGame />} />
+          <Route path="/bloom/test" element={<TestPage />} />
           {/*<Route path="/" element={<BloomGame />} />  Default route */}
         </Routes>
       </div>
---
+++ src/components/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestPage = () => {
+  return (
+    <div className="max-w-3xl mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-4">Welcome to Test Page</h1>
+      <p className="text-gray-700">This is a simple test page to demonstrate basic HTML structure.</p>
+    </div>
+  );
+};
+
+export default TestPage;
